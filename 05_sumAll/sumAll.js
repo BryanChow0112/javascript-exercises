2c2f632aca07e3ef1b8a678a4a5de923d67e5242
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-04 21:09:20 +0800

    Implement sumAll function with input validation and update tests to remove skips

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
-
-// Do not edit below this line
+const sumAll = function (min, max) {
+    // Check if both arguments are numbers
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    // Check if both numbers are positive
+    if (min < 0 || max < 0) return "ERROR";
+    // Check if min is greater than max and swap the values if needed
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+  
+    // An alternative way to swap the values of min and max like above is to use the array destructuring syntax.
+    // Here's an optional article on it: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+    // if (min > max) [min, max] = [max, min];
+    
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+  
 module.exports = sumAll;
